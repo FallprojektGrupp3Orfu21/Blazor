d1254fc046f33abbe819da895d88b4ecf9b0f70f
Pethaf <pethaf1@gmail.com>
2022-06-20 13:01:34 +0200

    Add razor page for recipient registration.

--- Economiq/Shared/DTO/RecipientDTO.cs
+++ Economiq/Shared/DTO/RecipientDTO.cs
@@ -1,10 +1,13 @@
-﻿namespace Economiq.Shared.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Economiq.Shared.DTO
 {
     public class RecipientDTO
     {
         public int Id { get; set;}
+        [Required(ErrorMessage = "Recipient Name Required")]
         public string Name { get; set; }
-
+        [Required(ErrorMessage ="Recipient City Required")]
         public string City { get; set; }
     }
 }
