a612c70c25458185b38e03dc49ebf38dc3901eea
Zeiyuri <zeyurigames@gmail.com>
2022-09-21 09:50:05 +0200

    Added implementation of remaining methods in BudgetService

--- Economiq/Server/Service/BudgetService.cs
+++ Economiq/Server/Service/BudgetService.cs
@@ -15,24 +15,67 @@ namespace Economiq.Server.Service
 
         public async Task<List<ListBudgetDTO>> GetAllBudgets(string username)
         {
-            return new();
+            User user = await GetUserByUsername(username);
+            if (user.Budgets.Any())
+            {
+                List<Budget> budgets = user.Budgets.OrderByDescending(b => b.EndDate).ToList();
+                List<ListBudgetDTO> budgetsDTO = new List<ListBudgetDTO>();
+
+                foreach(Budget budget in budgets)
+                {
+                    budgetsDTO.Add(new()
+                    {
+                        Id = budget.Id,
+                        MaxAmount = budget.MaxAmount,
+                        YearAndMonth = budget.StartDate.ToString("MMMM yyyy")
+                    });
+                }
+                return budgetsDTO;
+            }
+            else
+            {
+                return new List<ListBudgetDTO>();
+            }
         }
 
-        public async Task<List<ListBudgetDTO>> GetBudgetById(Guid id, string username)
+        public async Task<ListBudgetDTO> GetBudgetById(Guid id, string username)
         {
-            return new();
+            User user = await GetUserByUsername(username);
+            Budget? budget = user.Budgets.Where(b => b.Id == id).FirstOrDefault();
+            if(budget == null)
+            {
+                throw new Exception("Budget does not exist");
+            }
+            else
+            {
+                ListBudgetDTO budgetDTO = new()
+                {
+                    Id = budget.Id,
+                    MaxAmount = budget.MaxAmount,
+                    YearAndMonth = budget.StartDate.ToString("MMMM yyyy")
+                };
+                return budgetDTO;
+            }
         }
 
         public async Task<decimal> GetLatestMaxAmount(string username)
         {
-            //Return latest maxAmount or 0
-            return 0;
+            User user = await GetUserByUsername(username);
+            Budget? latestBudget = user.Budgets.OrderByDescending(b => b.EndDate).FirstOrDefault();
+            if (latestBudget == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return latestBudget.MaxAmount;
+            }
         }
 
         public async Task<ListBudgetDTO> GetBudgetByDate(CreateBudgetDTO budgetDTO, string username)
         {
-            var user = _context.Users.Where(u => u.UserName == username).Include(u => u.Budgets).FirstOrDefault();
-            if(DateTime.TryParse(budgetDTO.ExpenseDate, out DateTime date))
+            User user = await GetUserByUsername(username);
+            if (DateTime.TryParse(budgetDTO.ExpenseDate, out DateTime date))
             {
                 Budget? budget = user.Budgets.Where(b => b.StartDate <= date && date <= b.EndDate).FirstOrDefault();
                 if(budget != null)
@@ -45,19 +88,13 @@ namespace Economiq.Server.Service
                     };
                     return newBudgetDTO;
                 }
-                else
-                {
-                   return null;
-                }
             }
-            
-
-            return new();
+                return null;
         }
 
         public async Task CreateBudget(CreateBudgetDTO createBudgetDTO, string username)
         {
-            var user = _context.Users.Where(u => u.UserName == username).FirstOrDefault();
+            User user = await GetUserByUsername(username);
 
             if (DateTime.TryParse(createBudgetDTO.ExpenseDate, out DateTime date))
             {
@@ -86,6 +123,19 @@ namespace Economiq.Server.Service
             else
             {
                 throw new Exception("Could not parse expense date");
+            }
+        }
+
+        private async Task<User> GetUserByUsername(string username)
+        {
+            User? user = await _context.Users.Where(u => u.UserName == username).Include(u => u.Budgets).FirstOrDefaultAsync();
+            if(user != null)
+            {
+                return user;
+            }
+            else
+            {
+                throw new Exception("User not found");
             }
         }
     }
