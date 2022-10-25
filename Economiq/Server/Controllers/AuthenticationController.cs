cb81bad92c9da6c0372dbf34446c8b36b4aa3001
Zeiyuri <zeyurigames@gmail.com>
2022-10-25 11:54:32 +0200

    Basics committed for JWT to merge in main

---
+++ Economiq/Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,18 @@
+﻿using Economiq.Server.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Economiq.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly AuthenticationService _service;
+        public AuthenticationController(AuthenticationService service)
+        {
+            _service = service;
+        }
+
+    }
+}
